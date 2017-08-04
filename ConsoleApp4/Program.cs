f85ba39d68971a2b62757a2d717ef2b65a2fa91d
Antonio <antonio958@hotmail.com>
2017-08-04 12:44:21 +0200

    Acceso a datos

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
-namespace ConsoleApp4
+namespace AccesoDatos
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var connection = new SqlConnection("conexion");
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally {
+                if (connection.State == System.Data.ConnectionState.Open) {
+                    connection.Close();
+                }
+                connection.Dispose();
+            }
         }
     }
 }
